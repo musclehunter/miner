f8b7ebff101bdd82475402c3de5938c02f362afd
Masanobu Sato <masanobu.sato@thunderbringer.org>
2021-05-02 17:15:50 +0900

    Changes by bunom

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MineController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/mine', "App\Http\Controllers\MineController@index");
